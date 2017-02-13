8213a9de6a3b0eec3cd2dfdc449cdc473c49deaa
Trosydman <carlosmesquita94@outlook.es>
2017-02-13 10:25:50 +0100

    TrianaSat(0.2.1): añadiendo atributos en Timelapse.
    Se han añadido 2 atributos(uno modificado y otro nuevo): long
    fecha_captura y String fecha_subida(respectivamente).

--- TrianaSat/src/main/java/com/salesianostriana/trianasat/model/Timelapse.java
+++ TrianaSat/src/main/java/com/salesianostriana/trianasat/model/Timelapse.java
@@ -27,9 +27,13 @@ public class Timelapse {
 
 	// atributo tipo long, recoge la fecha y la hora tomada en cada instante del
 	// movil.
-	@Column
+	@Column(name="fecha_captura")
 	@NotNull
-	private long fecha;
+	private long fecha_captura;
+	
+	@Column(name="fecha_subida")
+	@NotNull
+	private String fecha_subida;
 	
 	@Column (name="foto")
 	@NotNull
@@ -50,9 +54,11 @@ public class Timelapse {
 	public Timelapse() {
 	}
 
-	public Timelapse(long id, long fecha, String foto, double estado_bateria, Proyecto proyecto) {
+	public Timelapse(long id, long fecha_captura, String fecha_subida, String foto, double estado_bateria,
+			Proyecto proyecto) {
 		this.id = id;
-		this.fecha = fecha;
+		this.fecha_captura = fecha_captura;
+		this.fecha_subida = fecha_subida;
 		this.foto = foto;
 		this.estado_bateria = estado_bateria;
 		this.proyecto = proyecto;
@@ -66,12 +72,20 @@ public class Timelapse {
 		this.id = id;
 	}
 
-	public long getFecha() {
-		return fecha;
+	public long getFecha_captura() {
+		return fecha_captura;
 	}
 
-	public void setFecha(long fecha) {
-		this.fecha = fecha;
+	public void setFecha_captura(long fecha_captura) {
+		this.fecha_captura = fecha_captura;
+	}
+
+	public String getFecha_subida() {
+		return fecha_subida;
+	}
+
+	public void setFecha_subida(String fecha_subida) {
+		this.fecha_subida = fecha_subida;
 	}
 
 	public String getFoto() {
@@ -100,10 +114,10 @@ public class Timelapse {
 
 	@Override
 	public String toString() {
-		return "Timelapse [id=" + id + ", fecha=" + fecha + ", foto=" + foto + ", estado_bateria=" + estado_bateria
-				+ ", proyecto=" + proyecto + "]";
+		return "Timelapse [id=" + id + ", fecha_captura=" + fecha_captura + ", fecha_subida=" + fecha_subida + ", foto="
+				+ foto + ", estado_bateria=" + estado_bateria + ", proyecto=" + proyecto + "]";
 	}
 
-	
+		
 
 }
